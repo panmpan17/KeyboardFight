b3e882156b57740607e070cb8d83d31524424436
Michael Pan <panmpan@gmail.com>
2019-06-13 14:23:10 +0800

    Mostly complete

---
+++ Assets/Script/WelcomPage.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WelcomPage : MonoBehaviour {
+	bool transitioning = false;
+	Animator anim;
+
+	void Awake () {
+		anim = GetComponent<Animator>();
+	}
+
+	public void Fight () {
+		if (!transitioning) {
+			anim.SetTrigger("FadeOut");
+			bool transitioning = true;
+		}
+	}
+
+	public void FinishedFadeOut () {
+		SceneManager.LoadScene("Game");
+	}
+}
